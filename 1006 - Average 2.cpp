f4d219d388920c7f1d49be70b99a70510aa609e9
Ashraful Alam <ashrafulalamyt@gmail.com>
2025-02-17 22:14:00 +0600

    1006 - Average 2 added

---
+++ 1006 - Average 2.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    float a, b, c; cin >> a >> b >> c;
+    cout << "MEDIA = " << fixed << setprecision(1) << (a*0.2 + b * 0.3 + c * 0.5) << endl;
+    return 0;
+}
