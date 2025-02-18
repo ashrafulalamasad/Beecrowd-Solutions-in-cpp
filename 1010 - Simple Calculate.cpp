fc401a49b75c0d265925d928fef023a784214f82
Ashraful Alam <ashrafulalamyt@gmail.com>
2025-02-18 07:41:17 +0600

    1010 - Simple Calculate added

---
+++ 1010 - Simple Calculate.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int m;
+    double a, b, c, d;
+    cin >> m >> a >> b;
+    cin >> m >> c >> d;
+    cout<<"VALOR A PAGAR: R$ "<<fixed<<setprecision(2)<<a*b+c*d<<endl;
+    return 0;
+}
