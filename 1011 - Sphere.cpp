1bd0828f845d177bc0ebba815cc89a991642e45c
Ashraful Alam <ashrafulalamyt@gmail.com>
2025-02-18 07:42:12 +0600

    1011 - Sphere added

---
+++ 1011 - Sphere.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double a, pi = 3.14159;
+    cin >> a;
+    cout<<"VOLUME = "<<fixed<<setprecision(3)<<(4*pi*pow(a, 3))/3<<endl;
+    return 0;
+}
