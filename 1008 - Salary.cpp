a73b3cb68ec9c095d620c95f883d7b5147902def
Ashraful Alam <ashrafulalamyt@gmail.com>
2025-02-18 07:39:08 +0600

    1008 - Salary added

---
+++ 1008 - Salary.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int m; double a, b; cin >> m >> a >> b;
+    cout << "NUMBER = " << m << endl;
+    cout << "SALARY = U$ " << fixed << setprecision(2) << a*b << endl;
+    return 0;
+}
